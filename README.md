38db7062d28c5b8e4d652e58fbd681f1ef2d285a
OOH <jeackross@gmail.com>
2017-12-10 16:29:56 +0100

    0.0.2 mongo

--- README.md
+++ README.md
@@ -5,6 +5,30 @@
 mongodb
 https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
 
+
+Open your terminal and run:
+
 npm -v 
 mongo --version
 
+
+
+--
+
+**npm install**
+
+# run
+
+**mongod**
+
+**npm run start**
+
+
+# errors
+
+- while running mongod: _complains that there is no /data/db folder_ [solved](https://stackoverflow.com/questions/7948789/mongodb-mongod-complains-that-there-is-no-data-db-folder)
+
+
+# test
+
+- so far by [Postman]()
--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    Task = require('./api/models/todoListModel'), //created model loading here
+    bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
 
 app.listen(port);
 
-console.log('*** REST API server started on: http://0.0.0.0:' + port);
+console.log('*** REST API server started on: http://localhost:' + port);
