4b1858c56202942920962912d6d75cdc2ffb8fc4
OOH <jeackross@gmail.com>
2017-12-11 14:29:13 +0100

    0.0.7 socket.io

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express'),
     app = express(),
     path = require('path'),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
     port = process.env.PORT || 3000,
     mongoose = require('mongoose'),
     Task = require('./api/models/todoListModel'), //created model loading here
@@ -13,7 +15,6 @@ var promise = mongoose.connect('mongodb://localhost/Tododb', {
     useMongoClient: true
 });
 
-
 app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'views')));
@@ -23,6 +24,55 @@ app.use(bodyParser.json());
 var routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app);
 
-app.listen(port);
+nicknames = {};
+connectCounter = 0;
 
-console.log('*** REST API server started on: http://localhost:' + port);
+function filterNullValues(i) {
+    return (i!=null);
+}
+
+io.on('connection', function(socket){
+    console.log(Object.keys(io.sockets.sockets).filter(filterNullValues).length);
+    //socket.join('all');
+    io.sockets.emit('connectCounter', Object.keys(io.sockets.sockets).filter(filterNullValues).length);
+    updateNicknames();
+
+//new user
+    socket.on('new user', function(data, callback){
+
+        callback(true);
+            socket.nickname = data;
+            nicknames[socket.nickname] = {online: true}; //Then we put an object with a variable online
+
+            updateNicknames();
+
+    });
+
+// update all user name
+
+    function updateNicknames(){
+        io.sockets.emit('usernames', nicknames);
+    }
+
+// send message
+
+    socket.on('send message', function(data){
+        io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+    });
+
+
+//disconnected service
+
+    socket.on('disconnect', function(data){
+        io.sockets.emit('connectCounter', Object.keys(io.sockets.sockets).filter(filterNullValues).length);
+        console.log(Object.keys(io.sockets.sockets).filter(filterNullValues).length);
+
+        if(!socket.nickname) return;
+        nicknames[socket.nickname].online = false; //We dont splie nickname from array but change online state to false
+        updateNicknames();
+    });
+});
+
+http.listen(3000, function(){
+    console.log('*** REST API server started on: http://localhost:' + port);
+});
--- views/scripts/index.js
+++ views/scripts/index.js
@@ -1,4 +1,77 @@
 var HW_config = {
-    selector: "#headwayapp", // CSS selector where to inject the badge
-    account:  "7qD1Bx"
-};
+        selector: "#headwayapp", // CSS selector where to inject the badge
+        account:  "7qD1Bx"
+    };
+
+    var parser = new UAParser();
+    var socket = io();
+
+    var result = parser.getResult();
+    var guid;
+
+    function uuidv4() {
+        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )}
+
+    if(Cookies.get('guid')){
+        guid = Cookies.get('guid');
+    } else {
+        Cookies.set('guid', uuidv4(),function () {
+            guid = Cookies.get('guid');
+        });
+    }
+
+    if(result.cpu.architecture === undefined || result.cpu.architecture === null){
+        result.cpu.architecture = result.os.version;
+    }
+
+    var device = {
+        name: result.os.name + " " + result.cpu.architecture,
+        browser:result.browser.name + " " + result.browser.version,
+        engine: result.engine.name + " " + result.engine.version
+
+    };
+
+    $('#send-message').submit(function(e){
+        e.preventDefault();
+        socket.emit('send message', $('#message').val());
+        $('#message').val('');
+    });
+
+    socket.on('new message', function(data){
+        $('#chat').append('<b>' + data.nick + ': </b>' + data.msg + "<br/>");
+    });
+
+    socket.on('connectCounter', function(data){
+        $('#connectCounter').empty();
+        $('#connectCounter').append("<b>ON-line: </b>" + data);
+    });
+
+    socket.emit('new user', device.name + " " + device.browser + " " + device.engine, function(data){
+
+            $('#contentWrap').show();
+
+    });
+
+    socket.on('usernames', function(data){
+        var html = '';
+        for (var nickname in data) //Foreach all nicknames received in data
+        {
+            var isNicknameOnline = data[nickname].online //Get the online state
+
+            //We test the online status
+            if (isNicknameOnline)
+            {
+                var status = '<font color=green><b>ON</b></font>'
+            }
+            else
+            {
+                var status = '<font color=red><b>OFF</b></font>'
+            }
+
+            html += status + ' ' + nickname + '<br/>' //print the status
+        }
+        $('#users').html(html);
+    });
+
