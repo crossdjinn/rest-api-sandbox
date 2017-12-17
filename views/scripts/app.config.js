c394c2e6c2a8608ef8336f00b4af6f914f48c3ee
OOH <jeackross@gmail.com>
2017-12-17 16:54:58 +0100

    0.1.9 titles

--- views/scripts/app.config.js
+++ views/scripts/app.config.js
@@ -4,18 +4,22 @@ config(['$locationProvider', '$routeProvider',
         $routeProvider.
         when('/', {
             template: '<home></home>',
-            activetab: 'home'
+            activetab: 'home',
+            activename: 'API routes'
         }).
         when('/task/new', {
             template: '<task-new></task-new>',
-            activetab: 'taskNew'
+            activetab: 'taskNew',
+            activename: 'New task'
         }).
         when('/task/list', {
             template: '<task-list></task-list>',
-            activetab: 'taskList'
+            activetab: 'taskList',
+            activename: 'All tasks'
         }).
         when('/task/:id', {
-            template: '<task-detail></task-detail>'
+            template: '<task-detail></task-detail>',
+            activename: 'Edit task'
         }).
         otherwise('/');
     }
